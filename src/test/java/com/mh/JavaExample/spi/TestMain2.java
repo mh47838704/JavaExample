7238676b35f8fbec9d81c1e77d400d90768d1d51
mh <47838704@163.com>
2018-05-01 23:00:48 +0800

    添加了SPI的jar包生成步骤

--- src/test/java/com/mh/JavaExample/spi/TestMain2.java
+++ src/test/java/com/mh/JavaExample/spi/TestMain2.java
@@ -7,6 +7,13 @@ import java.util.ServiceLoader;
 
 /**
  * 测试SPI
+ * 关于SPI的jar包的打包方式，可以参考oracle的官网说明：https://docs.oracle.com/javase/tutorial/sound/SPI-intro.html
+ * 本次SPI的测试jar包也是参考该说明打包的：
+ * 1、和spi相关的class文件拷贝到一个临时目录下/tmp(用户自定创建，和打包无关)
+ * 2、在该目录下创建META-INF目录，在META-INF目录下创建services目录，在目录下创建com.mh.JavaExample.spi.HelloService文件
+ * 3、在该文件中输入SPI接口的名字，如com.mh.JavaExample.spi.AmericaHelloService
+ * 4、进入到临时根目录/tmp中，执行命令jar cvf AmericaHelloService.jar -C . .
+ * 5、在该根目录下就可以看到AmericaHelloService.jar了
  * Start at: 2018/5/1 17:25
  *
  * @author muhong
