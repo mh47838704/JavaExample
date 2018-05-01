f7baa66dc3ac026a3935fc7f414b0253d665210f
mh <47838704@163.com>
2018-05-01 15:39:07 +0800

    创建JavaExample项目

---
+++ src/main/java/com/mh/JavaExample/JavaExampleApplication.java
@@ -0,0 +1,12 @@
+package com.mh.JavaExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaExampleApplication.class, args);
+	}
+}
